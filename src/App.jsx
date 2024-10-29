0ac711f1fc8b4cd4f14392cce29295ac5c0de4e5
projetos <claytonsalesdev@gmail.com>
2024-10-28 21:33:46 -0300

    .
    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
 
 import "./styles/App.css";
 
@@ -6,40 +8,68 @@ import { Navbar } from "./components/Navbar/Navbar";
 
 import { Article } from "./components/Article/Article";
 
-import articleimg1 from "./assets/img/dashboard.png";
+import { ThreeDots } from "react-loader-spinner";
 
-import articleimg2 from "./assets/img/img2.png";
+function App() {
+  const [news, setNews] = useState([]);
 
-import articleimg3 from "./assets/img/img3.png";
+  useEffect(() => {
+    async function loadnews() {
+      const response = await axios.get(
+        "https://api.spaceflightnewsapi.net/v3/articles"
+      );
+      const newsData = response.data;
 
-import articleimg4 from "./assets/img/img4.png";
-import { Counter } from "./components/Counter/Counter";
+      setNews(newsData);
+    }
 
+    loadnews();
 
-class App extends React.Component {
-  
-  render(){
+  }, []);
+
     return (
-      <>
-        <Navbar />
-
-        <Counter />
-
-        <section id="articles">
-
-        <Article title="Designing Dashboard" provider="NASA" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et numquam dolor voluptatibus repudiandae cum amet ea iste. Odit, commodi. Quidem, accusantium eaque nulla fuga delectus omnis numquam magnam quae consectetur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi, sed autem cumque unde architecto iste obcaecati ab voluptatibus quisquam impedit, voluptatem aliquam sunt ipsam consequuntur esse? Earum illum repellendus molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nemo, hic voluptatibus ut necessitatibus ipsum a, unde vero temporibus, ullam voluptas ratione in odio dolore deserunt? Veritatis alias eos error?
-        " thumbnail={articleimg1}/>
-
-        <Article title="Clayton" provider="Cerebro" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et numquam dolor voluptatibus repudiandae cum amet ea iste. Odit, commodi. Quidem, accusantium eaque nulla fuga delectus omnis numquam magnam quae consectetur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi, sed autem cumque unde architecto iste obcaecati ab voluptatibus quisquam impedit, voluptatem aliquam sunt ipsam consequuntur esse? Earum illum repellendus molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nemo, hic voluptatibus ut necessitatibus ipsum a, unde vero temporibus, ullam voluptas ratione in odio dolore deserunt? Veritatis alias eos error?" thumbnail={articleimg2}/>
-
-        <Article title="Bgaa Group" provider="Grupo" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et numquam dolor voluptatibus repudiandae cum amet ea iste. Odit, commodi. Quidem, accusantium eaque nulla fuga delectus omnis numquam magnam quae consectetur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi, sed autem cumque unde architecto iste obcaecati ab voluptatibus quisquam impedit, voluptatem aliquam sunt ipsam consequuntur esse? Earum illum repellendus molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nemo, hic voluptatibus ut necessitatibus ipsum a, unde vero temporibus, ullam voluptas ratione in odio dolore deserunt? Veritatis alias eos error?" thumbnail={articleimg3}/>
-
-        <Article title="Nivea Maria" provider="Prof" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Et numquam dolor voluptatibus repudiandae cum amet ea iste. Odit, commodi. Quidem, accusantium eaque nulla fuga delectus omnis numquam magnam quae consectetur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi, sed autem cumque unde architecto iste obcaecati ab voluptatibus quisquam impedit, voluptatem aliquam sunt ipsam consequuntur esse? Earum illum repellendus molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nemo, hic voluptatibus ut necessitatibus ipsum a, unde vero temporibus, ullam voluptas ratione in odio dolore deserunt? Veritatis alias eos error?" thumbnail={articleimg4}/>
-
-        </section>         
-      </>
-    );
-  }
+    <>
+     {/*<Counter />*/}
+      <Navbar />
+      
+      <section id="articles">
+        {news.length === 0 ? (
+          <div 
+          style={{
+            height: '400px', 
+            width: '100%', 
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center',
+          }}
+          >
+            <ThreeDots
+              height="80"
+              width="80"
+              radius="9"
+              color="white"
+              ariaLabel="loading"
+              wrapperStyle
+              wrapperClass
+            />
+        </div>
+          ) : (
+            news.map((article) => {
+              return (
+                <Article
+                  key={article.id}
+                  title={article.title}
+                  provider={article.newsSite}
+                  description={article.summary} 
+                  thumbnail={article.imageUrl}
+                />
+              );
+            })          
+          )
+        }
+      </section>         
+    </>
+  );
 }
 
 export default App;
